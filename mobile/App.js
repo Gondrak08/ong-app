87c2212a35604c48dfde5b653a800f39869c3d1f
Gondrak08 <vitor.alecrim.p@gmail.com>
2020-04-02 13:00:02 -0300

    aplha

---
+++ mobile/App.js
@@ -0,0 +1,26 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+import React from 'react';
+
+import Routes from './src/routes'
+
+export default function App() {
+  return (
+    <Routes />
+  );
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   title:{
+//     color: 'blue',
+//     fontSize: 20,
+//     fontWeight: '700',
+//   }
+// });
